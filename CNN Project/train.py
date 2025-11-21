6073656b7bf19f4ed601e10cbb35c9b119f249df
Jon Ibrani <ibranijon@gmail.com>
2025-11-21 20:09:47 +0100

    train structure

---
+++ CNN Project/train.py
@@ -0,0 +1,15 @@
+from sod_model import model,sod_loss,soft_IoU,optimizer
+from data_loader import create_dataset
+import tensorflow as tf
+
+def train_pass():
+    pass
+
+def val_pass():
+    pass
+
+def checkpoint():
+    pass
+
+def training():
+    pass
