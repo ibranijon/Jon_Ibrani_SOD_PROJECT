7ef53216130b31b91607c4529374a4b3d4e116de
Jon Ibrani <ibranijon@gmail.com>
2025-11-21 20:23:11 +0100

    components()

--- CNN Project/train.py
+++ CNN Project/train.py
@@ -2,6 +2,16 @@ from sod_model import model,sod_loss,soft_IoU,optimizer
 from data_loader import create_dataset
 import tensorflow as tf
 
+
+def components():
+    train_set, val_set, test_set = create_dataset()
+    sod_model = model()
+    opt = optimizer()
+
+    return train_set,val_set,test_set,sod_model,opt
+
+    
+
 def train_pass():
     pass
 
