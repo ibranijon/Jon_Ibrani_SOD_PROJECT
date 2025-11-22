bc653fbf5b9752c9ffeaadf4761c7c97185d8cba
Jon Ibrani <ibranijon@gmail.com>
2025-11-22 16:49:13 +0100

    compute metrics

--- CNN Project/evaluate.py
+++ CNN Project/evaluate.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib as plp
 
 from data_loader import create_dataset
-from sod_model import set_sod_model, optimizer, sod_loss, hard_IoU
+from sod_model import set_sod_model, optimizer
 
 
 #Model Prediction into the test set
@@ -12,10 +12,9 @@ from sod_model import set_sod_model, optimizer, sod_loss, hard_IoU
 
 def load_model_testset():
 
-    
     sod_model = set_sod_model()
     opt = optimizer()
-
+    
     checkp = tf.train.Checkpoint(model=sod_model,optimizer=opt,step=tf.Variable(0))
     manager = tf.train.CheckpointManager(checkp,'./checkpoints', max_to_keep=3)
     if manager.latest_checkpoint:
@@ -31,13 +30,46 @@ def load_model_testset():
 #Metric Computer 
 
 
-def compute_metric(sod_model, test_set):
+def compute_metrics(sod_model, test_set):
     precision_list = []
     recall_list = []
     f1_list = []
     mae_list = []
     iou_list = []
+    eps = 1e-7 
 
+
+    for image,masks in test_set:
+        mask_preds = sod_model(image, training=False)
+
+
+        #Binary Results of Prediction and Actual Data
+        mask_preds = tf.cast((mask_preds>0.5), tf.float32)
+        masks = tf.cast(masks,tf.float32)
+
+        #Calculation of TP,FP,TN,FN
+        TP = tf.reduce_sum(tf.cast((mask_preds==1 & masks == 1),tf.float32))
+        TN = tf.reduce.sum(tf.cast((mask_preds==0 & masks == 0),tf.float32))
+        FN = tf.reduce.sum(tf.cast((mask_preds==0 & masks == 1),tf.float32))
+        FP = tf.reduce.sum(tf.cast((mask_preds==1 & masks == 0),tf.float32))
+        
+        #Calculation of metrics
+
+        precision = TP/(TP + FP)
+        recall = TP/(TP + FN)
+        f1 = 2 * (precision*recall)/ (recall*precision)
+        iou = TP / (TP + FP + FN + eps)
+        mae = tf.reduce_mean(tf.abs(mask_preds-masks))
+        
+        #Filling information
+
+        precision_list.append(precision.numpy())
+        recall_list.append(recall.numpy())
+        f1_list.append(f1.numpy())
+        iou_list.append(iou.numpy())
+        mae_list.append(mae.numpy())
+
+    return precision_list, recall_list, f1_list, iou_list, mae_list
 
 
 
@@ -45,6 +77,8 @@ def compute_metric(sod_model, test_set):
 
 def evaluate():
     sod_model, test_set = load_model_testset()
+    precision_list, recall_list, f1_list, iou_list, mae_list = compute_metrics()
+
 
 
 #gathering of the confusion matrix elemetns 
